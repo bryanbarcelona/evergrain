e3d6a8f1a48f7159d78cb1228ef7ab57df09a727
Bryan Barcelona <131413239+bryanbarcelona@users.noreply.github.com>
2026-01-05 20:11:12 +0100

    chore: add automated versioning to __init__

--- src/evergrain/__init__.py
+++ src/evergrain/__init__.py
@@ -1,3 +1,10 @@
+import importlib.metadata
+
+try:
+    __version__ = importlib.metadata.version("evergrain")
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "unknown"
+
 from evergrain.core.metadata.engine import Metadata
 
 __all__ = ["Metadata"]
